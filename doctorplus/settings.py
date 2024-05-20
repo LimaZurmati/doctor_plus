c40547c00261613166e79f31c86d6dcc9af5e641
LimaZurmati <urzalalima@gmail.com>
2024-05-20 16:58:37 +0000

    Deployment settings

--- doctorplus/settings.py
+++ doctorplus/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@om3ynif1z#6^l0-us!+v+^@*4bmmsxulivcy*3w)qm70ouhe='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =     True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-limazurmati-doctorplus-jb3xjh9rnob.ws-us110.gitpod.io','.herokuapp.com']
 
