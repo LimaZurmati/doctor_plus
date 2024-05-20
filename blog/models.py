085f0d66d23a7af6b6190b73512e72c30bd96875
LimaZurmati <urzalalima@gmail.com>
2024-05-20 18:54:57 +0000

    Create model post and category

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category)
