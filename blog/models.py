c1744f9bcd656457fcaffdc900b6b1b13c6e6f7e
LimaZurmati <urzalalima@gmail.com>
2024-05-29 16:58:32 +0000

    Complate comment section

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,8 @@ class Post(models.Model):
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    #nationality = models.TextField()
+    #service = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     categories = models.ManyToManyField(Category)
