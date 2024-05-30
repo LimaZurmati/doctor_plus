eeee9f096eaaa7f584117a63a4db280c80aa5684
LimaZurmati <urzalalima@gmail.com>
2024-05-30 13:51:57 +0000

    Import  CloudinaryField from the cloudinary/models.py file

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -20,6 +21,7 @@ class Post(models.Model):
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    doctor_image = CloudinaryField('image', default='placeholder')
     #nationality = models.TextField()
     #service = models.CharField(max_length=200, unique=True)
     content = models.TextField()
